eecbc67442258c35a26a2584391c47f492bc3927
Antonkhl <54796806+Antonkhl@users.noreply.github.com>
2020-05-07 10:29:00 +0200

    Första namn metoden

--- src/hängaGubbeSpel/HängaGubbeSpel.java
+++ src/hängaGubbeSpel/HängaGubbeSpel.java
@@ -1,6 +1,30 @@
 package hängaGubbeSpel;
 
+import java.util.Scanner;
+
 public class HängaGubbeSpel {
 
+public static void main(String[] args) {
+	instruktionsNamn("name");
 }
-assdfsdfsdfsdfsdffasdfafsafafafafaf
+
+
+public static void instruktionsNamn(String namn) {
+	System.out.println("Hej och välkomna till det BÄSTA hänga gubbe spel, vi kommer verkligen testa dina kunskaper när det gäller meningar och ord.");
+	System.out.println("Jag kommer nu framöver i programet fråga dig/er några frågor för att forma denna hänge gubbespel på det sättet ni vill, och som passar er. Och sedan innan vi börjar ska jag förklara reglerna beroende på dom val ni har valt. ");
+	System.out.println("På vägen kommer jag ochså berätta relevans till allt jag frågar er");
+	System.out.println("Men innan vi börjar med all detta, var heter du?");
+	
+	Scanner input = new Scanner(System.in);
+	String name = input.nextLine();
+	
+	System.out.println("Hej " + name + "! Hoppas att du kommer gilla min program ");
+	
+	
+	
+	
+}
+	
+	
+	
+}
