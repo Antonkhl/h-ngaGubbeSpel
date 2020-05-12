f1fbc3ef6dee1da0da9701a6c72c3858cc869337
Antonkhl <54796806+Antonkhl@users.noreply.github.com>
2020-05-12 16:22:21 +0200

    defineTheWord methoden

--- src/hängaGubbeSpel/HängaGubbeSpel.java
+++ src/hängaGubbeSpel/HängaGubbeSpel.java
@@ -1,6 +1,7 @@
 package hängaGubbeSpel;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class HängaGubbeSpel {
 
@@ -80,17 +81,98 @@ public static void twoOnePlayers(String name, boolean oneOrTwoPlayers) {
 	}
 	    
 	    public static void selfDefiningWords(String name, boolean oneOrTwoPlayers, String secondPlayerName, boolean sentencesOrWords ) {
-	    	String svar1A = "Ord";
-			String svar1B = "ord";
-			String svar2A = "Mening";
-			String svar2B = "mening";
+	    	String svar1A = "Själv";
+			String svar1B = "själv";
+			String svar2A = "Biblotek";
+			String svar2B = "biblotek";
 	    	
-	    	System.out.println("Ok " + name + " och" + secondPlayerName + " Skulle ni vilja själva ange ordet som den andra ska gissa på eller välja bland en biblotek? " );
-            
+	    	System.out.println("Ok " + name + " och" + secondPlayerName + " Skulle ni vilja själva ange ordet som den andra ska gissa på eller välja bland en biblotek? (Själv/Biblotek) " );
+	    	
+	    	Scanner input = new Scanner(System.in);
+		    String svar = input.nextLine();
+		    
+		    if (svar==svar1B || svar==svar1A ) {
+		    	boolean selfDefining = false;
+		    	defineTheWord(name,true,secondPlayerName,true,false);
+		    	
+		    }
+		    
+		    if (svar==svar2B || svar==svar2A ) {
+		    	boolean selfDefining = true;
+		    }
+		    
+	    }
+	    	
+		    
+		    public static void defineTheWord(String name, boolean oneOrTwoPlayers, String secondPlayerName, boolean sentencesOrWords, boolean selfDefining) {
+		    	String svar1A = name;
+		    	String svar1B = name.substring(0, 1).toUpperCase() + name.substring(1);
+		    	String svar2A = secondPlayerName;
+		    	String svar2B = secondPlayerName.substring(0, 1).toUpperCase() + secondPlayerName.substring(1);
+		    	
+		    	System.out.println("Ok " + name + " och" + secondPlayerName + "Ni valde att själva definera ord!" );
+		    	System.out.println("Nu ska ni bestämma vem ska då definera ord för den andra! Jag kommer ge er nu 20 sekunder att bestäma vem det ska vara, kör igång!" );
+		    	System.out.println("Timern börjar.... NU" );
+
+		    	try {
+		    	TimeUnit.SECONDS.sleep(20);
+
+		    	}
+		    	
+		    	catch(Exception e) {
+		    		System.out.println("något gick fel");
+		    	}
+		    	
+		    	System.out.println("Ok! " + name + " och" + secondPlayerName + " Vem kommer definera orden? ( " + name + " / " + secondPlayerName + " ) ");
+		    	Scanner input = new Scanner(System.in);
+			    String svar = input.nextLine();
+			    
+			    
+			    if(svar == svar1A || svar == svar1B ) {
+			    	System.out.println("Ok! " + name + " Du får äran att välja ordet som " + secondPlayerName + " Ska gissa på!" );
+			    	System.out.println("Jag kommer ge dig " + name + " 30 sekunder för att komma på ett ord och för att " + secondPlayerName + " ska kolla bort!");
+			    	System.out.println("timern börjar.... NU");
+			    	
+			    	try {
+				    	TimeUnit.SECONDS.sleep(30);
+				    	}
+				    	
+				    	catch(Exception e) {
+				    		System.out.println("något gick fel");
+				    	}
+			    	
+			    	System.out.println("Ok! Nu hoppas jag att du " + name + "har hittat på ett ord, kan du ange det?");
+			    	String svar2 = input.nextLine();
+			    	
+			    	if(svar2.contains(" ")) {
+			    		System.out.println("Sorry! Men jag vill inte ha några mellanrum mellan ord! Om du vill köra med meningar får du starta om programet och välja meningar istället för ord");
+			    	}
+			    	
+			    	else {
+			    		String correctWord = svar2;
+			    		preMainGame(name,true,secondPlayerName,true,false,correctWord);
+			    		
+			    	}
+
+			    
+			    	
+			    }
+			    
+			    
+			    
+		    	}
+			    
+	
+		    	
+		    
+		    
+	    	
 			
 
 		
-	}
+		    public static void preMainGame(String name, boolean oneOrTwoPlayers, String secondPlayerName, boolean sentencesOrWords, boolean selfDefining, String correctWord) {
+	    		
+	    	}
 	
 	
 	
